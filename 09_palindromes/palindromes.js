1d8453b2f692a1c8b9cf6ed234b52af49eac8018
jkh-icon <jkh@icon.as>
2025-01-09 08:27:50 +0100

    Done 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const char = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+    const filteredString = string
+    .toLowerCase()
+    .split('')
+    .filter((characters) => char.includes(characters))
+    .join('');
+    
+    const reversedString = filteredString.split('').reverse().join('');
+
+    return filteredString === reversedString;
 };
+
+//palindromes('hello world');
 
 // Do not edit below this line
 module.exports = palindromes;
